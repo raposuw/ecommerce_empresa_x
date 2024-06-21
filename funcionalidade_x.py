3a8fdbac60005c4d80f3d8ec9d330ff81d932192
raposuw <andreychaves2009@hotmail.com>
2024-06-21 15:38:43 -0300

    Aumentando o tamaho da janela da funcionalidade X

--- funcionalidade_x.py
+++ funcionalidade_x.py
@@ -4,7 +4,7 @@ import tkinter as tk
 # Criar a janela
 root = tk.Tk()
 root.title("Minha Aplicação Customizada")
-root.geometry("400x300")
+root.geometry("600x400")
 
 # Adicionar widgets
 label = tk.Label(root, text="Olá, Mundo!")
